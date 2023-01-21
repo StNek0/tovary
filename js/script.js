706acdca72ab05f95f013d6a5f0bc66300cd1307
MS <samusevich21032007@gmail.com>
2023-01-21 13:50:08 +0300

    dz tovary

---
+++ js/script.js
@@ -0,0 +1,31 @@
+const URL = 'http://localhost:3000/TOVARS';
+const BUTTON = document.querySelector('.btn');
+const LIST = document.querySelector('.list');
+
+const getDatabase = async (url) => {
+    const res = await fetch(url);
+    const json = await res.json();
+    return json;
+}
+
+BUTTON.addEventListener('click', async () => {
+   let data = await getDatabase(URL);
+   LIST.innerHTML = '';
+   for (let key in data) {
+       LIST.insertAdjacentHTML('beforeend', `
+       <li>
+           <div class="info">
+               <img src="${data[key]["avatar"]}">
+               <span class="name">${data[key]["name"]}</span>
+           </div>
+           <div class="reactions">
+               <span class="views">${data[key]["cost"]}</span>
+               <span class="likes">${data[key]["number"]}</span>
+           </div>
+       </li>`);
+   }
+   BUTTON.innerHTML = 'Славься J!';
+   BUTTON.style.background = 'white';
+   BUTTON.style.color = 'black';
+   BUTTON.style.cursor = 'auto';
+});
